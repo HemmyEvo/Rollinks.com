23c36bf44f66c755a664c339bebb01cc7337625a
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-04-27 17:50:59 +0100

    Update ShoppingCartModal.tsx

--- app/(roots)/_components/ShoppingCartModal.tsx
+++ app/(roots)/_components/ShoppingCartModal.tsx
@@ -21,7 +21,7 @@ export default function ShoppingCartModal() {
 
     return (
       <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
-        <SheetContent className="w-full sm:max-w-lg bg-white/90 backdrop-blur-lg border-l border-green-50 shadow-xl">
+        <SheetContent className="w-full sm:max-w-lg bg-white/90 backdrop-blur-lg border-l border-green-50 shadow-xl" showX={false}>
           <motion.div 
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
