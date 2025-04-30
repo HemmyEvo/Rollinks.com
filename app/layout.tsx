9db64bf90a290e4834f67fad603bbbced41502a8
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-04-30 10:37:23 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,35 +5,43 @@ import Header from "./(roots)/_components/Header";
 import Footer from "./(roots)/_components/Footer";
 import CartProvider from "./(roots)/_components/Provider";
 import ShoppingCartModal from "./(roots)/_components/ShoppingCartModal";
-import {Toaster} from "react-hot-toast"
+import { Toaster } from "react-hot-toast";
 import { ConvexClientProvider } from "@/provider/ConvexProvider";
-import { ClerkLoading } from "@clerk/nextjs";
+import { ClerkLoading, ClerkLoaded } from "@clerk/nextjs";
 import Loading from "@/components/ui/Loading";
+
+// Load custom fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
 const gerald = localFont({
   src: "./fonts/GERALDINE PERSONAL USE.ttf",
   variable: "--font-genraldine",
   weight: "100 900",
 });
+
 const lover = localFont({
   src: "./fonts/LoversQuarrel-Regular.ttf",
   variable: "--font-lover",
   weight: "100 900",
 });
 
+// SEO Metadata
 export const metadata: Metadata = {
-  title: "Rollinks Store",
-  description: "This is a skincare store",
+  title: "Rollinks Skincare Store – Glow Starts Here",
+  description:
+    "Discover the science of radiant skin with Rollinks – your trusted store for premium, effective skincare solutions. Shop now for glowing, healthy skin backed by nature and innovation.",
 };
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -42,22 +50,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans} ${lover}   ${gerald} ${geistMono} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${gerald.variable} ${lover.variable} antialiased`}
       >
-        <ConvexClientProvider> 
+        <ConvexClientProvider>
           <ClerkLoading>
-          <Loading />  
-          </ClerkLoading>  
-        <CartProvider>
-        <Header />
-        <ShoppingCartModal />
-        <div className="py-16">
-        {children}
-</div>
-        <Footer/>
-        </CartProvider>
-       
-          <Toaster />
+            <Loading />
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <CartProvider>
+              <Header />
+              <ShoppingCartModal />
+              <main className="py-16">{children}</main>
+              <Footer />
+            </CartProvider>
+            <Toaster />
+          </ClerkLoaded>
         </ConvexClientProvider>
       </body>
     </html>
