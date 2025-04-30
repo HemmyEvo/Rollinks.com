b3b52c5c65f258f1c84d4331f7ee0b70e093843f
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-04-30 10:32:34 +0100

    Update Category.tsx

--- app/(roots)/_components/Category.tsx
+++ app/(roots)/_components/Category.tsx
@@ -29,7 +29,7 @@ export default function Category() {
   return (
     <div className="pb-[30px] pt-[56px] px-4 sm:px-12 bg-[#f8f8f8]">
       <header className="w-full md:max-w-4xl mx-auto space-y-5 text-center">
-        <div className="label flex items-center justify-center space-x-4">
+        <div className="label flex items-center space-x-4">
           <Image src="/Rectangle 18.png" width={12} height={12} alt="" />
           <p className="text-sm text-[#DB4444]">Categories</p>
         </div>
@@ -43,7 +43,7 @@ export default function Category() {
           {data.map((item) => (
             <Link 
               key={item._id}
-              href={`/products?category=${encodeURIComponent(item.name)}`}
+              href={`/product?category=${encodeURIComponent(item.name)}`}
               passHref
             >
               <div className="flex-shrink-0 w-28 h-28 flex flex-col items-center justify-center cursor-pointer bg-white rounded-lg shadow-md hover:shadow-lg transition-all hover:-translate-y-1">
