a82f0106db9bc482585f061ae65a8319c8cd0284
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-01 11:58:33 +0100

    Update interface.ts

--- app/interface.ts
+++ app/interface.ts
@@ -44,8 +44,8 @@ export interface fullProduct {
   category: CategoryReference;
   categoryName?: string; // This would be populated when joining data
   description: any[]; // Array of block content or images
-  ingredients?: string[];
-  benefits?: string[];
+  ingredients?: string | string[];
+  benefits?:string | string[];
   rating?: number;
   isNew: boolean;
   reviewCount: number;
