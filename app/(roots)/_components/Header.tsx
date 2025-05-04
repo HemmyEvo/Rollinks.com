e2e873856484fd2b0d19e2c3854104ba33d79b78
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-04 16:15:18 +0100

    Update Header.tsx

--- app/(roots)/_components/Header.tsx
+++ app/(roots)/_components/Header.tsx
@@ -105,7 +105,8 @@ const Header = () => {
               <motion.button
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
-                onClick={() => setShowSearch(!showSearch)}
+                onClick={() => { setIsToggle(false), setShowSearch(!showSearch)}}
+                
                 className="hidden md:flex p-2 rounded-full hover:bg-amber-50 transition-colors"
               >
                 <Search className="w-5 h-5 text-gray-700" />
