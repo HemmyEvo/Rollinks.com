73e9a4a1e775e4ecb2a94328aa12ff6399d379c5
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-13 13:20:30 +0100

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import category from "./category";
 import order from "./order";
 import product from "./product";
+import deliveryOption from "./deliveryOption"
 
-export const schemaTypes = [product, category,order]
+export const schemaTypes = [product, category,order,deliveryOption]
