72e79c76185bed812a5ec166fdb9f08b856d8da3
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-14 00:10:46 +0100

    Update CheckoutModal

--- app/(roots)/_components/CheckoutModal.tsx
+++ app/(roots)/_components/CheckoutModal.tsx
@@ -58,7 +58,7 @@ const [showModal, setShowModal] = useState(false);
   const [emailWarning, setEmailWarning] = useState(false);
 
   // Location state
-  const [selectedCountry, setSelectedCountry] = useState<Option | null>(null);
+  const [selectedCountry, setSelectedCountry] = useState<any| null>(null);
   const [selectedState, setSelectedState] = useState<Option[]| null>(null);
   const [stateOptions, setStateOptions] = useState<Option[]>([]);
   const [selectedLocation, setSelectedLocation] = useState<any | null>(null);
