b35469757ea6672dccdc3da852aa29420b781709
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-05-26 00:58:04 +0100

    Create ScrollToTopWrapper.tsx

---
+++ app/(roots)/_components/ScrollToTopWrapper.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ScrollToTopWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return <>{children}</>;
+}
